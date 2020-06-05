ff8f29adea46f19a6e5e52acd20842ff385d9747
Brian Fontaine <brian02440@live.fr>
2020-06-05 12:14:21 +0200

    exo

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+    include'../header.php';
+?>
+    <h1>Exercices 3</h1>
+    <p><?=$km = 1; $km;?><p>
+    <p><?=$km = 3; $km;?><p>
+    <p><?=$km = 125;$km;?><p>
+
+</body>
+<?php
+    include'../footer.php';
+?>
